127bfc3747ececfcd2cd4c7c270164b3b7be90fb
ntopower2 <n.to.the.power.of.2@gmail.com>
2020-03-27 09:39:08 +0000

    Stable version 1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#fp = open(input("Enter friendslist filename: "),'r')
+import lxml
 import pandas as pd
 friends = []
-fp = open("friendslist.txt",'r')
+fp = open("friendslist.txt",'r',encoding='latin1')
 lines = fp.readlines()
 for line in lines:
     pos=line.find("/profile")
@@ -12,6 +12,7 @@ for line in lines:
 url = r'https://cewl.a2hosted.com/lists.php?show=banned_members'
 tables = pd.read_html(url) # Returns list of all tables on page
 cewl_banned = tables[0]['PSN']
+print("Any banned members should appear here : ")
 for friend in friends:
     if len(cewl_banned[cewl_banned==friend]):
         print(friend)
